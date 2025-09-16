ffbc2756671e4c9ad35b3b87776e49fa11873c39
A-Pamimo <99601541+A-Pamimo@users.noreply.github.com>
2025-09-15 21:51:59 -0600

    Updated folder structure

--- R/99_session_info.R
+++
@@ -1,7 +0,0 @@
-
-# code/99_session_info.R --------------------------------------------------------
-suppressPackageStartupMessages({library(here)})
-sink(here::here("output","logs","session_info.txt"))
-print(sessionInfo())
-sink()
-cat("sessionInfo() written to output/logs/session_info.txt\n")
